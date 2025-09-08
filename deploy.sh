88bbddba6de43e472022b52a799350a3ad322978
Sergio <sergio@example.com>
2025-09-08 18:13:43 -0500

    v.20

--- deploy.sh
+++ deploy.sh
@@ -13,20 +13,6 @@ php artisan migrate --force
 # Enlace de storage (ignora error si ya existe)
 php artisan storage:link || true
 
-# Generar cachés para producción
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-
-
-
-# Limpiar cachés previas
-php artisan config:clear
-php artisan cache:clear
-php artisan route:clear
-php artisan view:clear
-
 
 # Mantener el servidor en ejecución en el puerto que asigna Railway
 php -S 0.0.0.0:$PORT -t public
-
