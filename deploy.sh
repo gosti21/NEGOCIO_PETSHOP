7535d4ab9c9d5e2ebda5cdddfd9b2d80e0647c52
Sergio <sergio@example.com>
2025-09-09 00:57:34 -0500

    ffas

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,15 @@ set -e
 
 export COMPOSER_ALLOW_SUPERUSER=1
 
-# Instalar dependencias de Composer
 composer install --no-dev --optimize-autoloader
 
-# Migraciones y cache de Laravel
 php artisan migrate --force || echo "⚠️ DB no disponible aún, migraciones omitidas"
 php artisan storage:link || true
 php artisan config:cache
 php artisan view:cache
 
-# Iniciar PHP-FPM y Nginx
-php-fpm -D
+# Iniciar PHP-FPM en background
+php-fpm -F &
+
+# Iniciar Nginx en foreground
 nginx -g "daemon off;"
