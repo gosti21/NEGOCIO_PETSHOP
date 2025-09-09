3f3885a63b45e05935eb59d9f63f946e33c05ed9
Sergio <sergio@example.com>
2025-09-08 19:50:34 -0500

    v.23

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,22 @@
 #!/bin/sh
 set -e
 
-# 🔹 Permitir plugins de Composer al correr como root en Railway
+# Permitir plugins de Composer al correr como root en Railway
 export COMPOSER_ALLOW_SUPERUSER=1
 
-# 🔹 Instalar dependencias optimizadas (sin dev)
+# Instalar dependencias optimizadas (sin dev)
 composer install --no-dev --optimize-autoloader
 
-# 🔹 Ejecutar migraciones forzadas
+# Ejecutar migraciones forzadas
 php artisan migrate --force
 
-# 🔹 Publicar assets de Livewire
-php artisan livewire:publish --assets
+npm install
+npm run build
 
-# 🔹 Enlace de storage (ignora error si ya existe)
+
+# Enlace de storage (ignora error si ya existe)
 php artisan storage:link || true
 
-# 🔹 Limpiar y generar cachés optimizadas para producción
-php artisan config:clear
-php artisan route:clear
-php artisan view:clear
-php artisan cache:clear
 
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-
-# 🔹 Mantener el servidor en ejecución en el puerto asignado por Railway
+# Mantener el servidor en ejecución en el puerto que asigna Railway
 php -S 0.0.0.0:$PORT -t public
